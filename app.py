3c530604283f5958798d65876e884ebaf97825c1
Kennysaruni <kennylatimore4@gmail.com>
2025-03-18 23:35:00 +0300

    Added feedback mechanism

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import numpy as np
 from twilio.rest import Client
 from tensorflow.keras.models import load_model
@@ -10,7 +12,9 @@ import os
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
 CORS(app)
+db = SQLAlchemy(app)
 
 # Load trained model
 model = load_model("spam_detector.keras")
@@ -28,6 +32,39 @@ TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
 
 # Initialize Twilio client
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    message = db.Column(db.String(500), nullable = False)
+    prediction = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message %r>' % self.id
+    
+@app.route('/feedback', methods =['POST'])    
+def feedback():
+      try:
+          
+          data = request.json
+
+          message = data.get('message')
+          prediction = data.get('prediction')
+
+          if not message or not prediction:
+              return jsonify({'error' : 'Missing message or prediction'}), 400
+          
+          feedback_entry = Message(message = message, prediction = prediction)
+
+          db.session.add(feedback_entry)
+          db.session.commit()
+
+          return jsonify({'message' : 'Feedback saved succesfully'}), 200
+      
+      except Exception as e:
+          return jsonify({'error' : 'There was an issue updating your feedback', 'details' : str(e)})
+
+
 
 def predict_message(pred_text):
     """Predict if a message is spam or ham."""
